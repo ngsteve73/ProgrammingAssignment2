36333af94113de1d4831034cec93ba0577a08b4f
Steve Ng <ngsteve@rocketmail.com>
2014-07-26 12:01:21 +0800

    Submission with comments
    Submission with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. The
+## following two functions are used to cache the inverse of a matrix.
+##
+## Example on usage of functions to calculate inverse of a matrix
+## 
+## First create a invertible square matrix that you want to find the inverse 
+## matrix of it as shown below for marix c
+##
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+##
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+##
+## Retrieving from the cache in the second run
+## cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+##
+## To prove that the answer is correct you can get the result and matrix multiply with 
+## the original matrix and you should get the identity matrix as shown below
+##
+## > y <- cacheSolve(m)
+## getting cached data
+## > x %*% y
+## [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object, and then cacheSolve 
+## calculates the inverse of the matrix.
+## If the matrix inverse has already been calculated, it will instead 
+## find it in the cache and return it, and not calculate it again.
 
+## makeCacheMatrix creates a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of inverse of the matrix
+## The argument x pass into the function is the matrix that you want to find the inverse of.
+## This matrix x is assumed to be invertible or a square matrix
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -18,8 +65,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns the inverse of the matrix. It first checks if
+## the inverse has already been computed. If so, it gets the result and skips the
+## computation. If not, it computes the inverse, sets the value in the cache via
+## setinverse function.
+## The argument x pass into the function is the list of functions return by the makeCacheMatrix above 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
@@ -33,3 +83,5 @@ cacheSolve <- function(x, ...) {
   x$setinverse(m)
   m
 }
+
+
